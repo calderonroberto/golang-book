60894f42325b1df2cfa4cc4a3c31eb66e4029986
Roberto <roberto@robertocalderon.ca>
2015-10-04 13:04:07 -0700

    fmted file

--- chapter8-pointers/main.go
+++ chapter8-pointers/main.go
@@ -2,19 +2,19 @@ package main
 
 import "fmt"
 
-func zero(x int){
+func zero(x int) {
 	x = 0
 }
 
-func zeroPtr (xPtr *int) { // *int specifies argument must be pointer
+func zeroPtr(xPtr *int) { // *int specifies argument must be pointer
 	*xPtr = 0
 }
 
-func one (xPtr *int) {
+func one(xPtr *int) {
 	*xPtr = 1
 }
 
-func main(){
+func main() {
 	x := 5
 	zero(x)
 	// Calling a function results in the argument being copied to
@@ -23,16 +23,14 @@ func main(){
 
 	// If we wanted to modify the original function here
 	// we can use what is known as a pointer:
-	zeroPtr(&x) // &x is the address of the variable x
+	zeroPtr(&x)    // &x is the address of the variable x
 	fmt.Println(x) // this time x is 0
-
 
 	// In go a pointer is represented using * followed type (*int)
 	// * is used to dereference pointer variables, *xPtr = 0 says
 	// "store the int 0 in the memory location xPtr refers to"
 	// the & operator is used to find the address of a variable &x
 	// so &x in main and xPtr in zero refer to the same memory location
-
 
 	// Another way to get a pointer is to use new, New takes a type
 	// and allocates enough memory to fit a value, returning a pointer
@@ -41,6 +39,5 @@ func main(){
 	xPtr := new(int)
 	one(xPtr)
 	fmt.Println(*xPtr) // x is 1
-
 
 }
