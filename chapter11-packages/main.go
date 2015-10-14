126cc5fc612e0378dabab301f7643b689925ee4c
Roberto <roberto@robertocalderon.ca>
2015-10-13 23:13:04 -0700

    added main program

--- chapter11-packages/main.go
+++ chapter11-packages/main.go
@@ -1,1 +1,10 @@
-importn "fmt"
+package main
+
+import "fmt"
+import "golang-book/chapter11-packages/math"
+
+func main() {
+	xs := []float64{1,2,3,4}
+	avg := math.Average(xs)
+	fmt.Println(avg)
+}
