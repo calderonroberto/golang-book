bfa90ad969c504ae4f6b2cf9188aeca59f52af21
Roberto <roberto@robertocalderon.ca>
2015-10-13 23:34:52 -0700

    added comments on godocs

--- chapter11-packages/math/math.go
+++ chapter11-packages/math/math.go
@@ -3,7 +3,18 @@ package math //package names generally match the folders they fall in.
 
 // To install this package run "go install" in this folder
 
-//public function when importing
+// public function when importing starts with Capitals
+
+// you can easily generate documentation with:
+//godoc github.com/calderonroberto/golang-book/chapter11-packages/math Average
+
+// even better.. you can generate web form docs with:
+//godoc -http:=":6060"
+// and visiting this url:
+// http://localhost:6060/pkg
+
+
+// Finds the average of a series of numbers (string in doc)
 func Average(xs []float64) float64 {
 	total := float64(0)
 	for _, x := range xs {
