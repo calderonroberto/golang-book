193a3f6305907f078267f3df825475a6e08fcbdc
Roberto <roberto@robertocalderon.ca>
2015-10-15 22:25:23 -0700

    added read dir

---
+++ chapter13-corepkg/readdir.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	// to open a directory pass a directory path
+	dir, err := os.Open(".")
+	if err != nil {
+		return
+	}
+	defer dir.Close()
+
+	fileInfos, err := dir.Readdir(-1)
+	if err != nil {
+		return
+	}
+	for _, fi := range fileInfos {
+		fmt.Println(fi.Name())
+	}
+}
