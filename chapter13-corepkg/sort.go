dee9d645f1a09c1d720b8ec077268beb9a5d45b3
Roberto <roberto@robertocalderon.ca>
2015-10-17 22:08:45 -0700

    added age example

--- chapter13-corepkg/sort.go
+++ chapter13-corepkg/sort.go
@@ -28,11 +28,25 @@ func (this ByName) Swap(i, j int) {
 // equivalent to a slice of what we want to sort.
 // We then define the 3 methods.
 
+type ByAge []Person
+
+func (this ByAge) Len() int {
+	return len(this)
+}
+func (this ByAge) Less(i, j int) bool {
+	return this[i].Age < this[j].Age
+}
+func (this ByAge) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
 func main() {
 	kids := []Person{
 		{"Jill", 9},
 		{"Jack", 10},
 	}
 	sort.Sort(ByName(kids))
+	fmt.Println(kids)
+	sort.Sort(ByAge(kids))
 	fmt.Println(kids)
 }
