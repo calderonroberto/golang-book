8fd1d6c0429803eb87717fa4ad2a07cc6082ce54
Roberto <roberto@robertocalderon.ca>
2015-10-17 22:03:10 -0700

    added example sort

---
+++ chapter13-corepkg/sort.go
@@ -0,0 +1,37 @@
+package main
+
+import ("fmt" ; "sort")
+
+type Person struct {
+	Name string
+	Age int
+}
+
+type ByName []Person
+
+func (this ByName) Len() int {
+	return len(this)
+}
+func (this ByName) Less(i, j int) bool {
+	return this[i].Name < this[j].Name
+}
+func (this ByName) Swap(i, j int) {
+	this[i], this[j] = this[j], this[i]
+}
+
+// The Sort function in sort takes a sort.Interface and sorts it. 
+// The sort Interface requires 3 methods: Len, Less and Swap. 
+// To define our own sort we create a new type (ByName) and make it 
+// equivalent to a slice of what we want to sort. 
+// We then define the 3 methods.
+
+
+
+func main() {
+	kids := []Person {
+		{"Jill", 9},
+		{"Jack", 10},
+	}
+	sort.Sort(ByName(kids))
+	fmt.Println(kids)
+}
